0442f47f199bb665bd465cd61bf66fbe202ee352
Elvis Wei <ewei068@berkeley.edu>
2021-10-14 15:07:08 -0700

    added lose scene, added bullet collision, added player health

---
+++ GGGproj3/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Starting player health")]
+    private float MaxHealth;
+    private float currHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = MaxHealth;
+    }
+
+    void TakeDamage(float damage)
+    {
+        currHealth -= damage;
+        if (currHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene("LoseScene");
+    }
+
+}

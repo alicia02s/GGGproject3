603e68181169e8ab4c232201bd0f890f0ee32199
Elvis Wei <ewei068@berkeley.edu>
2021-11-10 19:48:52 -0800

    hp slider

--- GGGproj3/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ GGGproj3/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -9,13 +10,17 @@ public class PlayerHealth : MonoBehaviour
     [Tooltip("Starting player health")]
     private float MaxHealth;
     private float currHealth;
+
+    private Slider slider;
     // Start is called before the first frame update
 
     // [SerializeField]
     // [Tooltip("Related to armor")]
-    private float DamageMitigation;
+    private float DamageMitigation = StaticVariableController.DamageMitigation;
     void Start()
     {
+        slider = FindObjectOfType<Slider>();
+        SetSliderValue(1);
         currHealth = MaxHealth;
     }
 
@@ -23,6 +28,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currHealth -= damage * DamageMitigation;
         Debug.Log(currHealth);
+        SetSliderValue(currHealth / MaxHealth);
         if (currHealth <= 0)
         {
             Die();
@@ -32,6 +38,14 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         SceneManager.LoadScene("RealLoseScene");
+    }
+
+    void SetSliderValue(float value)
+    {
+        if (slider != null)
+        {
+            slider.value = value;
+        }
     }
 
 }

29c6cb804428f66bffa013ae6c6982aab995e5ec
Elvis Wei <ewei068@berkeley.edu>
2021-12-03 19:45:48 -0800

    boss

---
+++ GGGproj3/Assets/Scripts/GiveSecondary.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveSecondary : MonoBehaviour
+{
+    /*[SerializeField]
+    [Tooltip("Tutorial zombie to spawn")]
+    private GameObject m_SecondaryPrefab;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            spawned = true;
+            Instantiate(m_EnemyPrefab, new Vector2(transform.position.x + 20, transform.position.y), transform.rotation);
+            Instantiate(m_EnemyPrefab, new Vector2(transform.position.x + 25, transform.position.y), transform.rotation);
+        }
+    } */
+}
